553973fd908a1c8d2f93b1609a0b6c503ab95668
Felipe Augusto <felipe112412@gmail.com>
2024-05-21 14:25:45 -0300

    Desafio Concluido

--- Desafio-1-Alura/appDesafio1.js
+++ Desafio-1-Alura/appDesafio1.js
@@ -1,1 +1,21 @@
-//teste
+alert ('Boas vindas ao nosso site');
+//let nome=lua;
+//let idade=25;
+let numeroDeVendas=50;
+let saldoDisponivel=1000;
+
+alert('Erro, preencha todos os campos');
+let mensagemDeErro = 'Erro, preencha todos os campos';
+alert(mensagemDeErro);
+
+let nome = prompt('Digite seu nome:');
+let idade = prompt('Digite sua idade:');
+if (idade >= 18)
+{
+    console.log ('Pode tirar habilitação');
+}
+else
+{
+    console.log ('Não pode tirar habilitação');
+}
+
