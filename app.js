b87e9c1e2f1828d62839c8cd55c526a902139474
Felipe Augusto <felipe112412@gmail.com>
2024-05-21 10:31:11 -0300

    Jogo Atualizado, Erro e Acerto

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-//TESTE
+alert('Bem Vindos ao jogo do Número Secreto');
+let numeroSecreto=5;
+let chute = prompt('Escolha um número entre 1 e 10')
+if (numeroSecreto==chute) 
+{
+    console.log('Você acertou :)')
+    alert('Você acertou :)')
+}
+else
+{
+    console.log('Você errou :(')
+    alert('Você errou :)')
+}
