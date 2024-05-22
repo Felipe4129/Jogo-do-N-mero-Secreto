c1ba66458ecc5b8f8ea6fb93b598d4239b5a5b12
Felipe Augusto <felipe112412@gmail.com>
2024-05-22 14:28:28 -0300

    Adicionado Desafio 2

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 alert('Bem Vindos ao jogo do Número Secreto');
-let numeroSecreto=5;
+let numeroSecreto=4;
+//console.log (NumeroSecreto);
 let chute = prompt('Escolha um número entre 1 e 10')
 if (numeroSecreto==chute) 
 {
-    console.log('Você acertou :)')
-    alert('Você acertou :)')
+    console.log('Você acertou o numero:');
+    alert(`Você acertou o numero secretooooo: ${numeroSecreto}`);
 }
 else
 {
-    console.log('Você errou :(')
-    alert('Você errou :)')
+    alert('Você errou :(')
 }
 
