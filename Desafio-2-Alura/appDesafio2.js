42b3487fd5bfa0ac642b5a1f0b69b000615082a9
Felipe Augusto <felipe112412@gmail.com>
2024-05-24 08:57:54 -0300

    Atualização Desafio 2

---
+++ Desafio-2-Alura/appDesafio2.js
@@ -0,0 +1,45 @@
+// 1) -----------------------
+
+/*
+var diaSemana = prompt("Qual é o dia da semana?");
+
+if (diaSemana == "sabado" || diaSemana == "domingo")
+{
+    alert('Bom fim de semana!');
+} else 
+{
+    alert('Boa semana!');
+}
+*/
+
+// 2) -----------------------
+
+/*var numero = prompt("Informe um número");
+
+if (numero>=0)
+{
+    alert('POSITIVO');
+}
+else
+{
+    alert('NEGATIVO');
+}
+*/
+
+// 3) -----------------------
+/*
+let pontuacao=100;
+var jogo = prompt("Informe a pontuacao obtida");
+
+if (jogo>=100)
+{
+    alert('Parabens voce venceu');
+}
+else
+{
+    alert('Tente novamente para ganhar');
+}
+*/
+
+// 4) -----------------------
+
