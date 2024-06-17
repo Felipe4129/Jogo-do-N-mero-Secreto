a9ae2e5cd69c6edbb35ec3cff117b82844188c55
Felipe Augusto <felipe112412@gmail.com>
2024-06-17 10:56:27 -0300

    Atualização do Jogo

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 alert('Bem Vindos ao jogo do Número Secreto');
-let numeroSecreto=5;
+let numeroSecreto= parseInt(Math.random () *100+1);
 console.log (numeroSecreto);
 let chute;
 let tentativas = 1;
 
 //enquanto chute não for igual ao numero secreto
 while (chute!=numeroSecreto)
-{
-    chute = prompt('Escolha um número entre 1 e 10');
-    //Se chute for igual numero secreto
+{   
+    chute = prompt ('Escolha um numero de 1 a 100');
+    //se chute for igual ao numero secreto
     if (chute == numeroSecreto) 
     {
-        alert(`Voce descobriu o numero secreto ${numeroSecreto} com ${tentativas} tentativas`);
+        break;
     } 
     else 
     {
@@ -24,6 +24,20 @@ while (chute!=numeroSecreto)
             alert(`O número secreto é maior que ${chute}`);
         }
         // tentativas = tentativas + 1;
-        tentativas++;
-    }
+        tentativas++;   
+    }   
 }
+
+let palavaTentativa=tentativas>1? 'tentativas' : 'tentativa';
+alert(`Voce descobriu o numero secreto ${numeroSecreto} com ${tentativas} ${palavaTentativa}.`);
+
+/*
+if(tentativas>1)
+{
+    alert(`Voce descobriu o numero secreto ${numeroSecreto} com ${tentativas} tentativas`);
+}
+else
+{
+    alert(`Voce descobriu o numero secreto ${numeroSecreto} com ${tentativas} tentativa`);
+}
+*/
