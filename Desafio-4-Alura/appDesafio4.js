8dda85c698fc4dd66cc2bdc4abf7614aef3d53d4
Felipe Augusto <felipe112412@gmail.com>
2024-07-05 11:11:58 -0300

    Atualização

---
+++ Desafio-4-Alura/appDesafio4.js
@@ -0,0 +1,45 @@
+
+// 1) -----------------------
+
+//*console.log ('Boas-Vindas')*//
+
+// 2) -----------------------
+
+//let nome = 'Felipe';
+//console.log (`Ola, ${nome}`);
+
+// 3) -----------------------
+
+//let nome = 'Felipe'
+//alert (`Ola ${nome}`)
+
+// 4) -----------------------
+
+//let qualLinguagem;
+//qualLinguagem = prompt('Qual linguagem voce mais gosta?');
+//alert(`A linguagem e ${qualLinguagem}`);
+
+// 5) -----------------------
+
+let valor1 = 20;
+let valor2 = 40;
+
+let resultado = valor1+valor2;
+alert (`O resultado de ${valor1} + ${valor2} e: ${resultado}`);
+
+
+// 6) -----------------------
+
+// 7) -----------------------
+
+// 8) -----------------------
+
+// 9) -----------------------
+
+// 10) -----------------------
+
+// 11) -----------------------
+
+// 12) -----------------------
+
+// 13 -----------------------
