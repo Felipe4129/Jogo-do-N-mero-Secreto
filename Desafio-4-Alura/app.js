5941cf3e5a5e1acef63e60c96b932074ddf25f6c
Felipe Augusto <felipe112412@gmail.com>
2024-07-05 10:02:58 -0300

    Atualização

---
+++ Desafio-4-Alura/app.js
@@ -0,0 +1,41 @@
+/*
+// 1) -----------------------
+
+let numero =1;
+while(numero<=10)
+{
+    console.log(numero);
+    numero++;
+}
+
+// 2) -----------------------
+
+let numero =10;
+while(numero>=0)
+{
+    console.log(numero);
+    numero--;
+}
+
+
+// 3) -----------------------
+
+let numero = parseInt(prompt('Escolha um número para começar a contagem regressiva:'));
+
+while (numero>=0)
+{
+    console.log(numero);
+    numero--;
+}
+
+// 4) -----------------------
+
+let numero = parseInt(prompt('Escolha um número para contagem progressiva:'));
+contagem = 0
+while (contagem<=numero)
+{
+    console.log(contagem);
+    contagem++;
+}
+
+*/
